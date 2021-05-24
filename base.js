a9b2e0fc6265e9934c5cc5065f18bb924ef6f9a8
Istvan Nemeth <istvnemeth1@gmail.com>
2021-05-24 12:45:14 +0100

    updated js

--- base.js
+++ base.js
@@ -1,15 +1,4 @@
-// var slider = document.getElementById("slider");
-// var slider_num = document.getElementById("slider_output");
 
-// slider.oninput = function(){
-//     checkSlider();
-// };
-// function checkSlider() {
-//     var slider_val = slider.val;
-//     slider_num.innerHTML = slider_val;
-//     console.log(slider_val);
-// };
-
-function() {
-    document.getElementById("slider_output").innerHTML = document.getElementById("slider").value;
+function updateOutput(val) {
+    document.getElementById("slider_output").innerHTML = val;
 };
