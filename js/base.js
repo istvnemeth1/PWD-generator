6af24fe728522767d88f2265d5947dc919738936
Istvan Nemeth <istvnemeth1@gmail.com>
2021-05-25 21:57:26 +0100

    copy button
    Working copy button in js/base.js

--- js/base.js
+++ js/base.js
@@ -1,4 +1,30 @@
-
+/* Shows the value of the slider */
 function updateOutput(val) {
     document.querySelector("#slider_output span").innerHTML = val;
+};
+
+/* Copy button */
+function copyButton(objId) {
+    deSelect();
+    if (document.selection) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(objId));
+        range.select();
+    }
+    else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(objId));
+        window.getSelection().addRange(range);
+    }
+    // objId.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand("copy");
+    var pwd = document.getElementById(objId).innerHTML;
+    alert("Copied the text: " + pwd);
+
+};
+function deSelect() {
+    if (document.selection)
+        document.selection.empty();
+    else if (window.getSelection)
+        window.getSelection().removeAllRanges();
 };
