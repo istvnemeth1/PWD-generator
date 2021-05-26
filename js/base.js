777247a3e5e7ae9d82077834117d1e8de2dead37
Istvan Nemeth <istvnemeth1@gmail.com>
2021-05-26 19:04:18 +0100

    Done
    Probably

--- js/base.js
+++ js/base.js
@@ -24,20 +24,50 @@ function deSelect() {
 };
 
 /* Shows the value of the slider */
+let pwdLength = 8;
 function updateOutput(val) {
     document.querySelector("#slider_output span").innerHTML = val;
+    pwdLength = val;
 };
 
 // Helper functions
 function random(val) {
-    Math.floor(Math.random()*val);
+    return Math.floor(Math.random()*val);
 };
 
 // Password Generation
 function generate() {
-    var checkBoxNum = Boolean;
-    var checkBoxLowC = Boolean;
-    var checkBoxUppC = Boolean;
-    var checkBoxSpecC = Boolean;
+    var password = "";
+    document.getElementById('textPassword').innerHTML = '';
+    const abc = "abcdefghijklmnopqrstuvwxyz";
+    const spec = "!£$%^&*~@:?><_+=-/.,#';";
+    const num = "1234567890";
+    // const cbuc = Array.from(abc.toUpperCase());
+    // const cblc = Array.from(abc);
+    // const cbn = Array.from(num);
+    // const cbsc = Array.from(spec);
+    const l_case = document.getElementById("l_case");
+    const u_case = document.getElementById("u_case");
+    const numbers = document.getElementById("numbers");
+    const s_char = document.getElementById("s_char");
+    var cbs = '';
+
+    if(l_case.checked || u_case.checked || numbers.checked || s_char.checked) {
+        l_case.checked ? cbs += abc:false;
+        u_case.checked ? cbs += abc.toUpperCase():false;
+        numbers.checked ? cbs += num:false;
+        s_char.checked ? cbs += spec:false;
+
+        pwdLength = Number(pwdLength);
+        var i;
+        for (i = 0; i < pwdLength; i++) {
+            password += cbs[random(cbs.length)];
+        }
+    } else {
+        alert("you need to check a checkbox.");
+    }
+    // console.log(document.getElementById('textPassword').innerHTML);
+    document.getElementById('textPassword').innerHTML = password;
+    console.log(password.length);
 }
 
